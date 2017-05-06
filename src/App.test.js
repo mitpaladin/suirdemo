9dabe7a8a111d9731889c4c261f2a01350350388
Jeff Dickey <jdickey@seven-sigma.com>
2017-05-07 01:27:34 +0800

    test(components) Add tests for ExampleCard
    Also add note to App.test.js noting that, since all we’re doing is demoing a (now-tested) presentational (dumb) component, that file contains only the most basic smoke test.
    
    Finally broke `AvatarImage` out into separate source file from that of `ExampleCard`. Fully presentational component; single prop/single path; too simple to test on its own.

--- src/App.test.js
+++ src/App.test.js
@@ -2,6 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
+/**
+ * NOTE: No "real" tests here yet, as all the App does is render a single, dumb
+ * component.
+ */
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
