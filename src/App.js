301f0ef64017489d8599296aa515ce6932eb78b4
Jeff Dickey <jdickey@seven-sigma.com>
2017-05-09 15:37:53 +0800

    feat(routing) Demo React Router v4 usage (#8)
    * Add simplest possible React Router POC code
    
    Currently, we route a request for `/card` to our old `ExampleCard` demo, and anything else to a barer-than-barebones “Not Found” component. Works exactly as expected.
    
    * Refactor existing AppHeader into inner components
    
    The point of this is to give us a simple, no-explicit-props-as-yet two-line `render` method for `AppHeader`.
    
    Now we can start adding app chrome. *Bwahahahahaaaaaaa…* 👿

--- src/App.js
+++ src/App.js
@@ -1,19 +1,36 @@
 
 import React, { Component } from 'react';
+import { Route, Switch } from 'react-router-dom'
+
 import AppHeader from './AppHeader';
 import ExampleCard from './ExampleCard';
 
 import './App.css';
+
+const DemoCard = () => (
+  <ExampleCard
+    avatar="matthew" name="Matthew" joinedYear={2015} job="a musician"
+    homeCity="Nashville" friendCount={22}
+  />
+)
+
+const NotFound = () => (
+  <div>
+    <h1>To infinity, and beyond!</h1>
+    <p>You are in a maze of twisty little passages, all alike. Go back, while
+    you still can. Try <code>/card</code> to see something else.</p>
+  </div>
+)
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <AppHeader />
-        <ExampleCard
-          avatar="matthew" name="Matthew" joinedYear={2015} job="a musician"
-          homeCity="Nashville" friendCount={22}
-        />
+        <Switch>
+          <Route path="/card" component={DemoCard} />
+          <Route path="*" component={NotFound} />
+        </Switch>
       </div>
     );
   }
