a42b002ad14619e67ced81d2266b79a082c29fa5
Jeff Dickey <jdickey@seven-sigma.com>
2017-05-09 15:29:21 +0800

    Refactor existing AppHeader into inner components
    The point of this is to give us a simple, no-explicit-props-as-yet two-line `render` method for `AppHeader`.
    
    Now we can start adding app chrome. *Bwahahahahaaaaaaa…* 👿

--- src/AppHeader.js
+++ src/AppHeader.js
@@ -4,19 +4,22 @@ import { Container, Header, Image } from 'semantic-ui-react';
 
 import logo from './logo.svg';
 
+const HeaderLogo = () => (
+  <Image src={logo} alt="logo" centered className="App-logo" />
+);
+
+const HeaderHeadline = () => (
+  <Header as="h1" inverted textAlign="center" className="App-header">
+    Welcome to Jeff&apos;s React Experiment
+  </Header>
+);
+
 class AppHeader extends Component {
   render() {
     return (
       <Container fluid className="App-header">
-        <Image src={logo} alt="logo" centered className="App-logo" />
-        <Header
-          as="h1"
-          inverted
-          textAlign="center"
-          className="App-header"
-        >
-          Welcome to Jeff&apos;s React Experiment
-        </Header>
+        <HeaderLogo />
+        <HeaderHeadline />
       </Container>
     );
   }
