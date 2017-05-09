684e6a5d621b89ff09d249f2d0d57d705f3fc60d
Jeff Dickey <jdickey@seven-sigma.com>
2017-05-09 15:26:12 +0800

    Add simplest possible React Router POC code
    Currently, we route a request for `/card` to our old `ExampleCard` demo, and anything else to a barer-than-barebones “Not Found” component. Works exactly as expected.

--- src/index.js
+++ src/index.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom'
+
 import App from './App';
 /* eslint-disable import/first */
 import './index.css';
 import 'semantic-ui-css/semantic.min.css';
 /* eslint-enable import/first */
 
-ReactDOM.render(
-  <App />,
+ReactDOM.render((
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>),
   document.getElementById('root'),
 );
 
