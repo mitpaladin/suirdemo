1a37ee714dd9f1a548f0d10597f2be9fdffeb48a
Jeff Dickey <jdickey@seven-sigma.com>
2017-05-11 15:00:08 +0800

    test(routing) Add Router context to App.test.js
    [Fixes #9]
    [Ping #1]

--- src/App.test.js
+++ src/App.test.js
@@ -1,12 +1,27 @@
+
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { MemoryRouter } from 'react-router-dom'
+import ReactTestUtils from 'react-dom/test-utils';
+
 import App from './App';
 
-/**
- * NOTE: No "real" tests here yet, as all the App does is render a single, dumb
- * component.
- */
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+it('renders NotFound component by default', () => {
+  const rendered = ReactTestUtils.renderIntoDocument(
+    <MemoryRouter initialEntries={[ '/' ]}>
+      <App />
+    </MemoryRouter>
+  );
+  const el = ReactTestUtils.findRenderedDOMComponentWithClass(rendered, 'not-found');
+  expect(el.tagName.toLowerCase()).toEqual('div');
+});
+
+it('renders ExampleCard component on path "/card"', () => {
+  const rendered = ReactTestUtils.renderIntoDocument(
+    <MemoryRouter initialEntries={[ '/card' ]}>
+      <App />
+    </MemoryRouter>
+  );
+  const el = ReactTestUtils.findRenderedDOMComponentWithClass(rendered, 'card');
+  expect(el.tagName.toLowerCase()).toEqual('div');
 });
